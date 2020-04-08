e7252246723f52fd737adc98bc6660cae75bf886
Rines.io <tsrines@gmail.com>
2020-04-08 11:40:40 -0400

    state sort

--- src/containers/StatesContainer.js
+++ src/containers/StatesContainer.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import StateCell from '../components/StateCell'
-import {Table, Button} from 'semantic-ui-react'
+import { Table, Form, Input } from 'semantic-ui-react'
+import _ from 'lodash'
 
 
 class StatesContainer extends React.Component {
 
-  state =  {states: [], search: ""}
+  state = {
+    states: [],
+    search: "",
+    column: null,
+    direction: null,
+  }
 
   getStates = () => {
     fetch(`https://corona.lmao.ninja/states`)
@@ -18,7 +24,7 @@ class StatesContainer extends React.Component {
 
   displayStates = () => {
     let states = this.state.states.filter(state => state.state.toLowerCase().includes(this.state.search.toLowerCase()))
-    states.sort((a,b) => b.cases - a.cases)
+
     let displayStates = states.map((state, index) => {
 
       return <StateCell key={index} {...state} />
@@ -28,27 +34,96 @@ class StatesContainer extends React.Component {
     return displayStates
   }
 
+  handleSearch = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
-  render (){
+  handleSort = (clickedColumn) => () => {
+    const { column, states, direction } = this.state
+
+    if (column !== clickedColumn) {
+      this.setState({
+        column: clickedColumn,
+        states: _.sortBy(states, [clickedColumn]),
+        direction: 'ascending',
+      })
+
+      return
+    }
+
+    this.setState({
+      states: states.reverse(),
+      direction: direction === 'ascending' ? 'descending' : 'ascending',
+    })
+  }
+
+
+  render() {
+    const { column, direction, search } = this.state
+
     return (
-      <Table singleLine>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>State</Table.HeaderCell>
-          <Table.HeaderCell>Cases {<Button size='mini' icon='arrow alternate circle down' />}</Table.HeaderCell>
-          <Table.HeaderCell>Cases Today{<Button
-            // onClick={() => this.addFavorite(this.vehicle)}
-            color={'twitter'}
-            icon='heart outline'
-          />}</Table.HeaderCell>
-          <Table.HeaderCell>Active Cases</Table.HeaderCell>
-          <Table.HeaderCell>Deaths</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {this.displayStates()}
-      </Table.Body>
-    </Table>
+      <div>
+        <Form>
+          <Input placeholder='Search...' type='text' name="search" value={search} onChange={(e) => this.handleSearch(e)}></Input>
+        </Form>
+
+        <Table sortable celled singleLine>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell
+                sorted={column === 'state' ? direction : null}
+                onClick={this.handleSort('state')}
+              >
+                State
+            </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'cases' ? direction : null}
+                onClick={this.handleSort('cases')}
+
+              >
+                Cases
+            </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'todayCases' ? direction : null}
+                onClick={this.handleSort('todayCases')}
+
+              >
+                Cases Today
+           </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'active' ? direction : null}
+                onClick={this.handleSort('active')}
+
+              >
+                Active Cases
+            </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'deaths' ? direction : null}
+                onClick={this.handleSort('deaths')}
+              >
+                Deaths
+          </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'tests' ? direction : null}
+                onClick={this.handleSort('tests')}
+              >
+                Tests
+          </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === 'testsPerOneMillion' ? direction : null}
+                onClick={this.handleSort('testsPerOneMillion')}
+              >
+                Tests Per One Million
+          </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {this.displayStates()}
+          </Table.Body>
+        </Table>
+      </div>
     )
   }
 }
