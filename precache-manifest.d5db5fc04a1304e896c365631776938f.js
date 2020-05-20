bed5563ba93b275e17123223e0b4863a2d421d27
Rines.io <tsrines@gmail.com>
2020-05-20 09:33:08 -0400

    Updates

---
+++ precache-manifest.d5db5fc04a1304e896c365631776938f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6896a031e7e8b0032c9b40ec4847bc02",
+    "url": "/corona-front/index.html"
+  },
+  {
+    "revision": "779580341ae5fc8796fb",
+    "url": "/corona-front/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "08ff3f0de40d8e0fa14c",
+    "url": "/corona-front/static/js/2.2e7a7f02.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/corona-front/static/js/2.2e7a7f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779580341ae5fc8796fb",
+    "url": "/corona-front/static/js/main.1576f414.chunk.js"
+  },
+  {
+    "revision": "7ea166fa9a8d420b80fb",
+    "url": "/corona-front/static/js/runtime-main.9c5b14a4.js"
+  }
+]);
